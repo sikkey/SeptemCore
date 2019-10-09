9b223c6286d560c81a03270f19140423c53adfbd
7Mersenne <7Mersenne@gmail.com>
2019-10-09 10:33:06 +0800

    fix buffer bug

--- Source/SeptemCore/Algorithm/SeptemBuffer.cpp
+++ Source/SeptemCore/Algorithm/SeptemBuffer.cpp
@@ -1,3 +1,19 @@
 // Copyright (c) 2013-2019 7Mersenne All Rights Reserved.
 
 #include "SeptemBuffer.h"
+
+int32 Septem::BufferBufferSyncword(uint8 * Buffer, int32 BufferSize, int32 Syncword)
+{
+	int32 index = 0;
+	int32 maxdex = BufferSize - 4;
+	while (index < maxdex)
+	{
+		int32* ptr = (int32*)(Buffer + index);
+		if (*ptr == Syncword)
+			return index;
+
+		++index;
+	}
+
+	return -1;
+}
--- Source/SeptemCore/Algorithm/SeptemBuffer.h
+++ Source/SeptemCore/Algorithm/SeptemBuffer.h
@@ -95,20 +95,7 @@ namespace Septem
 
 	// find the first syncword index in buffer
 	// return -1 or BufferSize when failed
-	static int32 BufferBufferSyncword(uint8* Buffer, int32 BufferSize, int32 Syncword)
-	{
-		int32 index = 0;
-		int32 maxdex = BufferSize - 4;
-		while (index < maxdex)
-		{
-			int32* ptr = (int32*)(Buffer + index);
-			if (*ptr == Syncword)
-				return index;
-
-			++index;
-		}
-
-		return -1;
-	}
+	int32 SEPTEMCORE_API BufferBufferSyncword(uint8* Buffer, int32 BufferSize, int32 Syncword);
+	
 }
 
